bb6bc7d7690ba241c93ac905e769f6f5f95ca9b4
Brandon <kounse@gmail.com>
2023-01-08 14:54:56 -0600

    Created validate_guess method for each guess. Logic ensures that guesses have to meet the parameters of the color selection.

---
+++ main.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+require './mastermind'
+
+game = Mastermind.new
+p game.hidden_code
+game.guess_hidden_code
--- mastermind.rb
+++ mastermind.rb
@@ -4,17 +4,30 @@ require 'colorize'
 
 # mastermind main class
 class Mastermind
-  attr_reader :hidden_code, :available_colors
+  attr_reader :hidden_code, :default_colors, :player, :computer
 
   def initialize
-    @available_colors = %w[white magenta red blue green yellow]
+    @default_colors = %w[white magenta red blue green yellow]
     set_hidden_code
+  end
+
+  def guess_hidden_code
+    guess = gets.chomp
+    guess_hidden_code if validate_guess(guess)
   end
 
   private
 
   def set_hidden_code
-    @hidden_code = @available_colors.dup
+    @hidden_code = @default_colors.dup
     @hidden_code.delete_at(rand(@hidden_code.length)) while @hidden_code.length > 4
+  end
+
+  def validate_guess(guess)
+    if guess.length != 4
+      true
+    else
+      guess.split('').any? { |num| p num.to_i < 1 || num.to_i > 6 } == true
+    end
   end
 end

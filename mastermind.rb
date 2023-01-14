10adc30a8c0dd45250df83a673d15cf59df9d7fd
Brandon <kounse@gmail.com>
2023-01-14 13:37:43 -0600

    Added a display information method for each round of gameplay. Added another error checker in invalid_guess? to check for alpha characters. Removed true return from second conditional check.

--- mastermind.rb
+++ mastermind.rb
@@ -13,6 +13,7 @@ class Mastermind
   end
 
   def play_mastermind
+    display_game_information
     guess_hidden_code
   end
 
@@ -26,6 +27,10 @@ class Mastermind
     print 'Please make your guess: '
     guess = gets.chomp
     guess_hidden_code if invalid_guess?(guess)
+  end
+
+  def display_game_information
+    puts "Current Turn: #{@current_turn} of 12"
   end
 
   private
@@ -48,9 +53,10 @@ class Mastermind
   end
 
   def invalid_guess?(guess)
-    if guess.length != 4
+    if guess.split('').any? { |alpha| ('a'..'z').cover?(alpha) }
+      puts 'Please enter only numbers!'
+    elsif guess.length != 4
       puts 'Please enter 4 numbers!'
-      true
     elsif guess.split('').any? { |num| num.to_i < 1 || num.to_i > 6 } == true
       puts 'Numbers have to be 1 through 6 only!'
     end

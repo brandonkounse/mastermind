eb2a0f455033a3d222d279ec26c22c662d848781
Brandon <kounse@gmail.com>
2023-01-14 22:33:16 -0600

    Reworded and refined the instruction set for readability.

--- instructions.rb
+++ instructions.rb
@@ -4,21 +4,20 @@
 module Instructions
   def instructions
     <<~HEREDOC
-      Welcome to Mastermind!
+      \nWelcome to Mastermind!
 
       To win, you must guess the correct colors in the correct order. There are six available colors
       that can make up the hidden code: 1-white \e[35m2-magenta\e[0m \e[31m3-red\e[0m \e[34m4-blue\e[0m \e[32m5-green\e[0m \e[33m6-yellow\e[0m.
 
-      If the hidden code is [white, \e[31mred\e[0m, \e[34mblue\e[0m, \e[33myellow\e[0m], you would have to type in 1346 for that selection.
+      If the hidden code is: white | \e[31mred\e[0m | \e[34mblue\e[0m | \e[33myellow\e[0m you would have to type in 1346 for that selection.
 
-      Each guess made will be provided feedback on it's accuracy. A white circle will indiciate that
-      a correct color was guessed in the right location. An empty circle will indicicate that a
-      correct color was guessed, but in the wrong location.
+      Each guess will be provided feedback on it's accuracy.
+      ●  means a correct color was guessed in the right location.
+      ○  means a correct color was guessed, but in the wrong location.
+      The order of the feedback circles **DO NOT** correspond to their location in the hidden code.
 
-      For example, if we use the hidden code above [white, \e[31mred\e[0m, \e[34mblue\e[0m, \e[33myellow\e[0m]
-      and we guessed 3145, we would see this result: ● ○ ○
-
-      The order of the feedback circles do not correspond to their location in the hidden code.\n
+      For example, in the previous hidden code: white | \e[31mred\e[0m | \e[34mblue\e[0m | \e[33myellow\e[0m
+      if we guessed 3145 for \e[31mred\e[0m | white | \e[34mblue\e[0m | \e[32mgreen\e[0m] we would see this result: ● ○ ○\n
     HEREDOC
   end
 end

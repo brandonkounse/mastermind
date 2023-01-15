0d67faed1461d04e8991944b28cc3b54ac6c8c78
Brandon <kounse@gmail.com>
2023-01-15 12:29:38 -0600

    Re-added :name attribute to class.

--- player.rb
+++ player.rb
@@ -2,9 +2,10 @@
 
 # class for the codebreaker and codemaker
 class Player
-  attr_reader :role
+  attr_reader :name, :role
 
-  def initialize(role)
+  def initialize(role, name)
     @role = role
+    @name = name
   end
 end

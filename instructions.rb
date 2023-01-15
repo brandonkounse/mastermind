f789e709a37de5dafc064715839bc6cc9c210f04
Brandon <kounse@gmail.com>
2023-01-15 14:46:51 -0600

    Reworking project to utilize color names instead of numbers for selection. Updating docs accordingly.

--- instructions.rb
+++ instructions.rb
@@ -9,7 +9,8 @@ module Instructions
       To win, you must guess the correct colors in the correct order. There are six available colors
       that can make up the hidden code: 1-white \e[35m2-magenta\e[0m \e[31m3-red\e[0m \e[34m4-blue\e[0m \e[32m5-green\e[0m \e[33m6-yellow\e[0m.
 
-      If the hidden code is: white | \e[31mred\e[0m | \e[34mblue\e[0m | \e[33myellow\e[0m you would have to type in 1346 for that selection.
+      If the hidden code is: white | \e[31mred\e[0m | \e[34mblue\e[0m | \e[33myellow\e[0m you would have to type in
+      each color in the right order. Example: "white" "red" "blue" "yellow".
 
       Each guess will be provided feedback on it's accuracy.
       ●  means a correct color was guessed in the right location.
@@ -17,7 +18,7 @@ module Instructions
       The order of the feedback circles **DO NOT** correspond to their location in the hidden code.
 
       For example, in the previous hidden code: white | \e[31mred\e[0m | \e[34mblue\e[0m | \e[33myellow\e[0m
-      if we guessed 3145 for \e[31mred\e[0m | white | \e[34mblue\e[0m | \e[32mgreen\e[0m] we would see this result: ● ○ ○\n
+      if we guessed \e[31mred\e[0m | white | \e[34mblue\e[0m | \e[32mgreen\e[0m we would see this result: ● ○ ○\n
     HEREDOC
   end
 end

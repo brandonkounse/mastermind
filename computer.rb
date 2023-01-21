2aa2667006e1fc34b6e481fe2782d07af6f0fd59
Brandon <kounse@gmail.com>
2023-01-21 15:41:54 -0600

    Added a computer class to serve as the opponenet for the player.

---
+++ computer.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require './player'
+
+# class for the computer opponent to play against
+class Computer < Player
+  attr_accessor :role
+
+  def initialize; end
+end

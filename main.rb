0cb0dda56b49883f4810541e31f51d84295bb03d
Brandon <kounse@gmail.com>
2023-01-23 20:18:01 -0600

    Fixed bug where game would end on turn 11 of 12 instead of 12 of 12.

--- main.rb
+++ main.rb
@@ -10,7 +10,7 @@ game.add_player(Player.new)
 game.add_opponent(Computer.new)
 game.setup
 
-while game.turn < Mastermind::MAX_TURNS
+while game.turn <= Mastermind::MAX_TURNS
   game.play
   break if game.over == true
 end

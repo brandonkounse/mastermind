271ed4eb20e2c2ee1d8160a26a745a14d3fd0b4d
Brandon <kounse@gmail.com>
2023-01-23 20:24:02 -0600

    Removed initialize method.

--- computer.rb
+++ computer.rb
@@ -6,5 +6,12 @@ require './player'
 class Computer < Player
   attr_accessor :role
 
-  def initialize; end
+  def guess
+    random_colors = []
+    until random_colors.length == 4
+      random_guess = rand(6)
+      random_colors.push(random_guess) unless random_colors.include?(random_guess)
+    end
+    random_colors
+  end
 end
